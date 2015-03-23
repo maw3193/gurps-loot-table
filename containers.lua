dd3fb1de2314e5598842ab5d2da70a9457223b97
Jonathan Maw <jonathan.maw@gmail.com>
2015-03-23 17:56:13 +0000

    Fix soft containers

--- containers.lua
+++ containers.lua
@@ -3,7 +3,7 @@ require "helpers"
 containers_table = {
     ["1, 1-2"] = {name="Amphora", cost=60, weight=40, soft=false},
     ["1, 3-4"] = {name="Barrel", cost=65, weight=20, soft=false},
-    ["1, 5-6"] = {name="Basket", cost=1.75, weight=1.5, soft=false},
+    ["1, 5-6"] = {name="Basket", cost=1.75, weight=1.5, soft=true},
     ["2, 1-2"] = {name="Bottle/Jar", cost=3, weight=1, soft=false},
     ["2, 3-4"] = {name="Bottle/Jar, Small", cost=2, weight=0.5, soft=false},
     ["2, 5"]   = {name="Box, Stone, Large", cost=1000, weight=220, soft=false},
@@ -24,7 +24,7 @@ containers_table = {
     ["6, 1"]   = {name="Strongbox, Iron, Compartmentalized", cost=300, weight=18, soft=false},
     ["6, 2"]   = {name="Vial, Crystal", cost=5, weight=0.25, soft=false},
     ["6, 3"]   = {name="Vial, Iron", cost=15, weight=0.5, soft=false},
-    ["6, 4"]   = {name="Wineskin, 1 Gallon", cost=10, weight=0.25, soft=false},
+    ["6, 4"]   = {name="Wineskin, 1 Gallon", cost=10, weight=0.25, soft=true},
     ["6, 5"]   = {name="Cushioned", cf=1},
     ["6, 6"]   = {name="Secret Compartment", cf=2}
 }
@@ -32,3 +32,7 @@ containers_table = {
 expand_table(containers_table)
 
 container_vial = containers_table["6, 2"]
+container_bottle_small = containers_table["2, 3"]
+container_bottle = containers_table["2, 1"]
+container_barrel = containers_table["1, 3"]
+container_box = containers_table["3, 1"]
